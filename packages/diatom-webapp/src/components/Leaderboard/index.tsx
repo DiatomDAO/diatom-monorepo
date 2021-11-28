c66434fac657ac3cf46c6439bf8d91bee6484da5
themetabolic <themetabolic@protonmail.com>
2021-11-28 17:33:10 -0300

    Reformulating the empty Leaderboard placeholder, adding ghost whale.

--- packages/diatom-webapp/src/components/Leaderboard/index.tsx
+++ packages/diatom-webapp/src/components/Leaderboard/index.tsx
@@ -1,9 +1,8 @@
+import ghostWhale from '../../assets/ghostWhale.jpg'
 import classes from './Leaderboard.module.css';
 
 const Leaderboard = () => {
-  const whales = [
-    { name: 'Placeholder', plasticRemoved: 300000.00123 },
-  ].sort((a, b) => {
+  const whales = [{ name: 'Placeholder', plasticRemoved: 0 }].sort((a, b) => {
     if (a.plasticRemoved < b.plasticRemoved) return 1;
     if (a.plasticRemoved > b.plasticRemoved) return -1;
     return 0;
@@ -23,19 +22,25 @@ const Leaderboard = () => {
           <p>Est. Plastic Removed</p>
         </div>
         <div className={classes.leaderboardList}>
-          {whales.length > 1 && whales.map((whale, index) => {
-            return (
-              <div key={index} className={classes.leaderboardItem}>
-                <p className={classes.whalePosition}>#{index + 1}</p>
-                <div></div>
-                <p className={classes.whaleName}>{whale.name}</p>
-                <p>{formatter(whale.plasticRemoved)} Kg</p>
-              </div>
-            );
-          })}
-          {whales.length <= 1 && 
-            <p className={classes.noWhales}>{`No Whales on the Leaderboard just yet!`}</p>
-          }
+          {whales.length > 1 &&
+            whales.map((whale, index) => {
+              return (
+                <div key={index} className={classes.leaderboardItem}>
+                  <p className={classes.whalePosition}>#{index + 1}</p>
+                  <div></div>
+                  <p className={classes.whaleName}>{whale.name}</p>
+                  <p>{formatter(whale.plasticRemoved)} Kg</p>
+                </div>
+              );
+            })}
+          {whales.length <= 1 && (
+            <div className={classes.leaderboardItem}>
+              <p className={classes.whalePosition}>#{'??'}</p>
+              <img src={ghostWhale} alt="Ghost Whale" />
+              <p className={classes.whaleName}>{'??????'}</p>
+              <p>{'???.???'} Kg</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
