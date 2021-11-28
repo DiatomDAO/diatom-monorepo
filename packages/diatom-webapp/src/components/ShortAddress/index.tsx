adf23efa85bad14528e2b4df87334cf8846dbd4e
themetabolic <themetabolic@protonmail.com>
2021-11-28 17:15:00 -0300

    Making the bidder name shorter.

--- packages/diatom-webapp/src/components/ShortAddress/index.tsx
+++ packages/diatom-webapp/src/components/ShortAddress/index.tsx
@@ -9,12 +9,14 @@ const ShortAddress: React.FC<{ address: string; avatar?: boolean }> = props => {
 
   const ens = useReverseENSLookUp(address);
   const shortAddress = address && [address.substr(0, 4), address.substr(38, 4)].join('...');
+  const shortEns =
+    ens && ens.length > 10 ? [ens.substr(0, 4), ens.substr(ens.length - 5, 4)].join('...') : ens;
 
   if (avatar) {
     return (
-      <div className={classes.shortAddress}>
+      <div className={classes.shortAddress} title={ens ? ens : shortAddress}>
         {avatar && <Davatar size={24} address={address} provider={provider} />}
-        {ens ? ens : shortAddress}
+        {ens ? shortEns : shortAddress}
       </div>
     );
   }
