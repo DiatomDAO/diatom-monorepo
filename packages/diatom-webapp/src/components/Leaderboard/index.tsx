0843abe689120e0acfb1c8f50af56c88d4be91f3
themetabolic <themetabolic@protonmail.com>
2021-12-01 17:55:23 -0300

    Adding static fix to the Leaderboard.

--- packages/diatom-webapp/src/components/Leaderboard/index.tsx
+++ packages/diatom-webapp/src/components/Leaderboard/index.tsx
@@ -1,17 +1,21 @@
-import ghostWhale from '../../assets/ghostWhale.jpg'
+// import ghostWhale from '../../assets/ghostWhale.jpg';
 import classes from './Leaderboard.module.css';
 
 const Leaderboard = () => {
-  const whales = [{ name: 'Placeholder', plasticRemoved: 0 }].sort((a, b) => {
+  const whales = [{ id: 1, name: 'Rainbow Warrior', plasticRemoved: 0 }].sort((a, b) => {
     if (a.plasticRemoved < b.plasticRemoved) return 1;
     if (a.plasticRemoved > b.plasticRemoved) return -1;
     return 0;
   });
 
-  const formatter = (amount: number | bigint) =>
-    new Intl.NumberFormat('en-US', {
+  const formatter = (amount: number | bigint) => {
+    if (amount === 0) {
+      return '???'
+    }
+    return new Intl.NumberFormat('en-US', {
       maximumFractionDigits: 0,
     }).format(amount);
+  }
 
   return (
     <div className={classes.leaderboardContainer}>
@@ -22,25 +26,24 @@ const Leaderboard = () => {
           <p>Est. Plastic Removed</p>
         </div>
         <div className={classes.leaderboardList}>
-          {whales.length > 1 &&
-            whales.map((whale, index) => {
-              return (
-                <div key={index} className={classes.leaderboardItem}>
-                  <p className={classes.whalePosition}>#{index + 1}</p>
-                  <div></div>
-                  <p className={classes.whaleName}>{whale.name}</p>
-                  <p>{formatter(whale.plasticRemoved)} Kg</p>
-                </div>
-              );
-            })}
-          {whales.length <= 1 && (
+          {whales.map((whale, index) => {
+            return (
+              <div key={whale.id} className={classes.leaderboardItem}>
+                <p className={classes.whalePosition}>#{index + 1}</p>
+                <img src={`/static/images/whalesIcon/whale${whale.id}.jpg`} alt="Whale Icon" />
+                <p className={classes.whaleName}>{whale.name}</p>
+                <p>{formatter(whale.plasticRemoved)} Kg</p>
+              </div>
+            );
+          })}
+          {/* {whales.length <= 1 && (
             <div className={classes.leaderboardItem}>
               <p className={classes.whalePosition}>#{'??'}</p>
               <img src={ghostWhale} alt="Ghost Whale" />
               <p className={classes.whaleName}>{'??????'}</p>
               <p>{'???.???'} Kg</p>
             </div>
-          )}
+          )} */}
         </div>
       </div>
     </div>
