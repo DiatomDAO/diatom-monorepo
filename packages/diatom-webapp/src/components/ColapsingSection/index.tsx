56555b3cebf93f6d2055e10b97e8bb9abacc56e7
not-mrrobot <mrrobordao@pm.me>
2021-12-04 00:03:18 -0600

    addresses changes

--- packages/diatom-webapp/src/components/ColapsingSection/index.tsx
+++ packages/diatom-webapp/src/components/ColapsingSection/index.tsx
@@ -22,11 +22,6 @@ const ColapsingSection = () => {
         <h2>Diatom DAO</h2>
         <div className={classes.contentContainer}>
           <p>
-            Diatom leverages the power of staking and bonding pioneered by OlympusDAO to fund
-            highly-vetted ocean protection projects, including the first ever tokenization of ocean
-            plastic removal.
-          </p>
-          <p>
             Holders of the governance token, DIAT, will be able to suggest and vote on Diatom DAO
             proposals in a community-driven governance model.
           </p>
@@ -44,7 +39,7 @@ const ColapsingSection = () => {
         <div className={classes.contentContainer}>
           <p>
             The winners of each auction receive both the NFT, and an amount of our DAO token, $DIAT,
-            commensurate with their bid amount. This priced at $25, the same price as our
+            commensurate with their bid amount. This is priced at $25, the same price as our
             whitelist token sale, and the lowest at which it will ever be sold.
           </p>
           <p>
@@ -69,17 +64,14 @@ const ColapsingSection = () => {
         <h2>Auctions</h2>
         <div className={classes.contentContainer}>
           <p>
-            The Diatom Auction Contract will auction one Whalez NFT linearly decaying auction slots
-            over the period of 2 weeks, beginning with 14 hours and diminishing until all 50 are
-            won. The auctions will end on or around December 14th{' '}
-            <strong>
-              (not considering time spent waiting for a settler or the accumulation of 5 minute
-              timer resets if bids are placed in the last 5 minutes of an auction)
-            </strong>
-            . 100% of auction proceeds (ETH) are automatically deposited in the DiatomDAO treasury,
-            where 65% will be immediately used to fund plastic removal projects and mint the first
-            PRCs. The remaining 35% will remain in the treasury for the DAO to decide how to
-            allocate.
+            The Diatom Auction Contract will auction one Whalez NFT over linearly decaying auction
+            slots over the period of 2 weeks, beginning with 14 hours and diminishing until all 50
+            are won. The auctions will end on or around December 14th (not considering time spent
+            waiting for a settler or the accumulation of 5 minute timer resets if bids are placed in
+            the last 5 minutes of an auction) . 100% of auction proceeds (ETH) are automatically
+            deposited in the DiatomDAO treasury, where 65% will be used to fund plastic removal
+            projects and mint the first PRCs. The remaining 35% will remain in the treasury for the
+            DAO to decide how to allocate.
           </p>
           <p>
             Each time an auction is settled, the settlement transaction will also cause a new Whalez
@@ -95,12 +87,18 @@ const ColapsingSection = () => {
         <div className={classes.contentContainer}>
           <p>
             Whalez are the expression of the critical role that whales play in the global ecosystem
-            (see our Medium article for more), and are an invitation for crypto Whales to prove
-            crypto's true capacity for social impact (Whalez4Whales).
+            (see our{' '}
+            <a
+              href="https://medium.com/@diatomdao/introducing-diatom-dao-c9f7dd17b34c"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Medium article
+            </a>{' '}
+            for more), and are an invitation for crypto Whales to prove crypto's true capacity for
+            social impact (Whalez4Whales).
           </p>
-          <p style={{ marginBottom: '6px' }}>
-            Whalez NFT's have several unique characteristics:
-          </p>
+          <p style={{ marginBottom: '6px' }}>Whalez NFT's have several unique characteristics:</p>
           <div className={classes.list}>
             <p>- Unique name</p>
             <p>- Unique phrase</p>
@@ -109,9 +107,7 @@ const ColapsingSection = () => {
             <p>- Eye expressions (10 forms, varying colors)</p>
             <p>- Backgrounds (4 forms, varying colors)</p>
           </div>
-          <p>
-          The name and phrase is connected to the Whalez personality and unique lore.
-          </p>
+          <p>The name and phrase is connected to the Whalez personality and unique lore.</p>
         </div>
       </div>
     </div>
