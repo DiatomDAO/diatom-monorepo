81e030468156b7ba5462594fb6fb8f5f93b94e36
themetabolic <themetabolic@protonmail.com>
2021-12-04 17:04:13 -0300

    Adding old hero to the DAO page

--- packages/diatom-webapp/src/pages/Dao/index.tsx
+++ packages/diatom-webapp/src/pages/Dao/index.tsx
@@ -1,9 +1,11 @@
 import DiatomInfo from '../../components/DiatomInfo';
 import InfoSections from '../../components/InfoSections';
+import LandingHero from '../../components/LandingHero';
 import Tokenomics from '../../components/Tokenomics';
 
 const DaoPage = () => (
   <div>
+    <LandingHero />
     <DiatomInfo />
     <Tokenomics />
     <InfoSections />
