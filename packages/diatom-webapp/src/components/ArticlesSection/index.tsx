a5dabf599c1eeff9bc4d71c534bd0b6e4d6b6412
themetabolic <themetabolic@protonmail.com>
2022-01-31 15:17:47 -0300

    Article styles finished and actual articles titles and images added

--- packages/diatom-webapp/src/components/ArticlesSection/index.tsx
+++ packages/diatom-webapp/src/components/ArticlesSection/index.tsx
@@ -1,4 +1,6 @@
 import pollutingRivers from './assets/pollutingRivers.jpg';
+import schoolOfFish from './assets/schoolOfFish.jpg';
+import removingPlastic from './assets/removingPlastic.jpg';
 
 import classes from './ArticlesSection.module.css';
 
@@ -15,20 +17,20 @@ const ArticlesSection = () => (
         <img src={pollutingRivers} alt="Polluted River" />
       </a>
       <a
-        href="https://medium.com/@diatomdao/diatoms-investment-strategy-7b01aa6abdec"
+        href="https://medium.com/@diatomdao/introducing-diatom-dao-c9f7dd17b34c"
         target="_blank"
         rel="noreferrer"
       >
         <p>Diatom DAO is Scouting 100 Polluting Rivers in Indonesia with Sungai Watch</p>
-        <img src={pollutingRivers} alt="Polluted River" />
+        <img src={schoolOfFish} alt="Polluted River" />
       </a>
       <a
-        href="https://medium.com/@diatomdao/diatoms-investment-strategy-7b01aa6abdec"
+        href="https://medium.com/@diatomdao/plastic-reduction-credits-prc-whitepaper-pt-1-3-b88c3d6e1171"
         target="_blank"
         rel="noreferrer"
       >
         <p>Diatom DAO is Scouting 100 Polluting Rivers in Indonesia with Sungai Watch</p>
-        <img src={pollutingRivers} alt="Polluted River" />
+        <img src={removingPlastic} alt="Polluted River" />
       </a>
     </div>
   </div>
