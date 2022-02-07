478d1c302c3750438147b544230a6c2e22b69b72
themetabolic <themetabolic@protonmail.com>
2022-02-07 02:26:16 -0300

    Rebranding 'how it works' section

--- packages/diatom-webapp/src/components/DiatomInfo/index.tsx
+++ packages/diatom-webapp/src/components/DiatomInfo/index.tsx
@@ -2,12 +2,14 @@ import WhatIsDiatom from "./WhatIsDiatom";
 import Lungs from "./Lungs";
 import PlasticCredits from "./PlasticCredits";
 import ArticlesSection from "../ArticlesSection";
+import InfoSections from "../InfoSections";
 
 const DiatomInfo = () => (
   <>
     <WhatIsDiatom />
     <ArticlesSection />
     <Lungs />
+    <InfoSections />
     <PlasticCredits />
   </>
 );
--- packages/diatom-webapp/src/pages/Dao/index.tsx
+++ packages/diatom-webapp/src/pages/Dao/index.tsx
@@ -1,6 +1,5 @@
 import AdivisorsSection from '../../components/AdvisorsSection';
 import DiatomInfo from '../../components/DiatomInfo';
-import InfoSections from '../../components/InfoSections';
 import LandingHero from '../../components/LandingHero';
 import Roadmap from '../../components/Roadmap';
 import Tokenomics from '../../components/Tokenomics';
@@ -10,7 +9,6 @@ const DaoPage = () => (
     <LandingHero />
     <DiatomInfo />
     <Tokenomics />
-    <InfoSections />
     <AdivisorsSection />
     <Roadmap />
   </div>
